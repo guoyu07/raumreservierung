0895154a25d0a807779e2577683f153e473b9f3c
moritzmenzel <moritzmenzel@users.noreply.github.com>
2017-03-07 19:17:30 +0100

    Improved SW Scripts

--- precache-config.js
+++ precache-config.js
@@ -3,37 +3,6 @@
  */
 
 module.exports = {
-    staticFileGlobs: [
-        "index.html",
-
-        "src/raumreservierung-main/raumreservierung-main.html",
-
-        "src/raumreservierung-admin/raumreservierung-admin.html",
-        "src/raumreservierung-admin/src/admin-error-reports/admin-error-reports.html",
-        "src/raumreservierung-admin/src/admin-nutzerverwaltung/admin-nutzerverwaltung.html",
-        "src/raumreservierung-admin/src/admin-raumverwaltung/admin-raumverwaltung.html",
-        "src/raumreservierung-email-confirmation/raumreservierung-email-confirmation.html",
-        "src/raumreservierung-landingpage/raumreservierung-landingpage.html",
-        "src/raumreservierung-login/raumreservierung-login.html",
-        "src/reporting-form/reporting-form.html",
-        "src/raumreservierung-privacy/raumreservierung-privacy.html",
-        "src/raumreservierung-activate-account/raumreservierung-activate-account.html",
-        "src/raumreservierung-imprint/raumreservierung-imprint.html",
-        "src/raumreservierung-reset-password/raumreservierung-reset-password.html",
-        "src/raumreservierung-account-page/raumreservierung-account-page.html",
-        "src/raumreservierung-admin/src/admin-cronjobs/admin-cronjobs.html",
-        "src/raumreservierung-teacher/raumreservierung-teacher.html",
-        "src/raumreservierung-teacher/teacher-datepicker.html",
-        "src/raumreservierung-teacher/teacher-raumreservierung.html",
-        "src/raumreservierung-teacher/teacher-raumreservierung-roomlist.html",
-        "src/raumreservierung-teacher/teacher-reservation-request.html",
-        "src/raumreservierung-teacher/teacher-reservations.html",
-        "src/raumreservierung-teacher/teacher-timelist.html",
-        "src/raumreservierung-admin/src/admin-raumverwaltung/admin-reservation-control.html",
-
-        "img/*",
-        "src/raumreservierung-admin/img/*"
-    ],
     importScripts: [
         "sw-update.js"
     ]
--- sw-update.js
+++ sw-update.js
@@ -3,12 +3,23 @@
  */
 
 function update(request, cacheName) {
-    console.log("[service-worker.js] file-update called");
-    return caches.open(cacheName).then(function(cache) {
-        return fetch(request).then(function(response) {
-            return cache.put(request, response.clone()).then(function() {
-                return response;
+    if('onLine' in navigator) {
+        if(navigator.onLine) {
+            return caches.open(cacheName).then(function(cache) {
+                return fetch(request).then(function(response) {
+                    return cache.put(request, response.clone()).then(function() {
+                        return response;
+                    });
+                });
+            });
+        }
+    } else {
+        return caches.open(cacheName).then(function(cache) {
+            return fetch(request).then(function(response) {
+                return cache.put(request, response.clone()).then(function() {
+                    return response;
+                });
             });
         });
-    });
+    }
 }
