6c207be5eda2c1a6e7122a68cb8a800bc9df1c1b
moritzmenzel <moritzmenzel@users.noreply.github.com>
2017-03-08 18:22:30 +0100

    Added images and img - directories, as well as npm module sources to the precache-config

--- precache-config.js
+++ precache-config.js
@@ -4,6 +4,20 @@
 
 module.exports = {
     importScripts: [
-        "sw-update.js"
+        "js/sw-update.js",
+        "js/offline-analytics.js"
+    ],
+    staticFileGlobs: [
+        "favicon.ico",
+        "favicon.png",
+        "mstile-144.png",
+        "apple-touch-icon.png",
+        "img/*",
+        "src/raumreservierung-admin/img/*",
+        "js/*",
+
+        /** NPM Node Modules */
+
+        "node_modules/sw-offline-google-analytics/build/offline-google-analytics-import.js"
     ]
 };
