59e6e47cda31df9e6f1957e752a71454c16935a6
moritzmenzel <moritzmenzel@users.noreply.github.com>
2017-03-08 18:21:56 +0100

    Added offline-analytics - script to be imported by the service-worker.js

---
+++ js/offline-analytics.js
@@ -0,0 +1,6 @@
+/**
+ * Created by moritz on 08.03.17.
+ */
+
+importScripts('node_modules/sw-offline-google-analytics/build/offline-google-analytics-import.js');
+goog.offlineGoogleAnalytics.initialize();
