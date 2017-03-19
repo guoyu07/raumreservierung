e8d7bddc1535d3fb7f0976bc20b930a2aadcbd9f
moritzmenzel <moritzmenzel@users.noreply.github.com>
2017-03-19 18:47:31 +0100

    Added relative paths for Mr. S. :D

--- precache-config.js
+++ precache-config.js
@@ -5,22 +5,55 @@
 module.exports = {
     importScripts: [
         /** Excluded, cause the function is not needed atm */
-        // "js/sw-update.js",
-        "js/offline-analytics.js"
+        // "./js/sw-update.js",
+        "./js/offline-analytics.js"
     ],
     staticFileGlobs: [
-        "favicon.ico",
-        "favicon.png",
-        "mstile-144.png",
-        "apple-touch-icon.png",
-        "img/*",
-        "src/raumreservierung-admin/img/*",
-        "js/*",
 
-        /** NPM Node Modules */
+        // Precache resources apart from Polymer fragments
+        "./favicon.ico",
+        "./favicon.png",
+        "./mstile-144.png",
+        "./apple-touch-icon.png",
+        "./img/*",
+        "./src/raumreservierung-admin/img/*",
+        "./js/*",
+        "./LICENSE.txt",
 
-        "node_modules/sw-offline-google-analytics/build/*"
+        // NPM Node Modules
+        "./node_modules/sw-offline-google-analytics/build/*",
+
+        /* UPDATE FOR SCHOOL WEBSERVER */
+        // Precache of resources with absolute paths
+        //
+        "./index.html",
+        // FRAGMENTS
+        "./src/raumreservierung-admin/raumreservierung-admin.html",
+        "./src/raumreservierung-admin/src/admin-error-reports/admin-error-reports.html",
+        "./src/raumreservierung-admin/src/admin-nutzerverwaltung/admin-nutzerverwaltung.html",
+        "./src/raumreservierung-admin/src/admin-raumverwaltung/admin-raumverwaltung.html",
+        "./src/raumreservierung-email-confirmation/raumreservierung-email-confirmation.html",
+        "./src/raumreservierung-landingpage/raumreservierung-landingpage.html",
+        "./src/raumreservierung-login/raumreservierung-login.html",
+        "./src/reporting-form/reporting-form.html",
+        "./src/raumreservierung-privacy/raumreservierung-privacy.html",
+        "./src/raumreservierung-activate-account/raumreservierung-activate-account.html",
+        "./src/raumreservierung-imprint/raumreservierung-imprint.html",
+        "./src/raumreservierung-reset-password/raumreservierung-reset-password.html",
+        "./src/raumreservierung-account-page/raumreservierung-account-page.html",
+        "./src/raumreservierung-admin/src/admin-cronjobs/admin-cronjobs.html",
+        "./src/raumreservierung-teacher/raumreservierung-teacher.html",
+        "./src/raumreservierung-teacher/teacher-datepicker.html",
+        "./src/raumreservierung-teacher/teacher-raumreservierung.html",
+        "./src/raumreservierung-teacher/teacher-raumreservierung-roomlist.html",
+        "./src/raumreservierung-teacher/teacher-reservation-request.html",
+        "./src/raumreservierung-teacher/teacher-reservations.html",
+        "./src/raumreservierung-teacher/teacher-timelist.html",
+        "./src/raumreservierung-admin/src/admin-raumverwaltung/admin-reservation-control.html",
+        // APP SHELL
+        "./src/raumreservierung-main/raumreservierung-main.html"
     ],
     /** Fallback as replace for the .htaccess 404 redirect */
-    navigateFallback: '/index.html'
+    navigateFallback: 'index.html',
+    directoryIndex: 'index.html'
 };
